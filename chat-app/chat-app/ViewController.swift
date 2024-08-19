a8c367a0156efc49c689d82006c1a53eac9d5b22
Niño Castorico <bncast@gmail.com>
2024-08-19 09:23:00 +0800

    initialize xcode files

---
+++ chat-app/chat-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-app
+//
+//  Created by Niño Castorico on 8/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
