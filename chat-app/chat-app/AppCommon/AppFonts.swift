403f946c5e8e72cd459da38ea929ad2a2b9f3fb3
Ramon Jr Bahio <ramonbah.kor@gmail.com>
2024-08-19 11:17:17 +0800

    added typography file, colors file and colors asset

---
+++ chat-app/chat-app/AppCommon/AppFonts.swift
@@ -0,0 +1,31 @@
+//
+//  AppFonts.swift
+//  chat-app
+//
+//  Created by Ramon Jr Bahio on 8/19/24.
+//
+
+import UIKit
+
+extension UIFont {
+    /// Font size: 33, weight: Regular
+    static var largeTitle: UIFont {
+        .systemFont(ofSize: 33)
+    }
+
+    /// Font size: 21, weight: Regular
+    static var sectionTitle: UIFont {
+        .systemFont(ofSize: 21)
+    }
+
+    /// Font size: 19, weight: Bold
+    static var titleBold: UIFont {
+        .systemFont(ofSize: 19, weight: .bold)
+    }
+
+    /// Font size: 19, weight: Regular
+    static var caption: UIFont {
+        .systemFont(ofSize: 19)
+    }
+}
+
